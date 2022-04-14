b84e8be984095334acb0273bc454b27a723494a9
APOLLO08 <99278749+APOLLO08@users.noreply.github.com>
2022-04-14 20:55:26 +0700

    Update zombie.php

--- zombie.php
+++ zombie.php
@@ -1,6 +1,6 @@
 {
 "Version_update": "1.0",
-"Link_zip": "https://github.com/APOLLO08/zombievpn/blob/main/zombie.zip?raw=true",
+"Link_zip": "https://github.com/APOLLO08/zombievpn/raw/main/zombie.zip",
 "Password": "254500",
 "Message": "⚠️ควรอ่านรายละเอียดก่อนใช้งาน
 รายละเอียดการอัปเดตวันที่ 15-04-2022
